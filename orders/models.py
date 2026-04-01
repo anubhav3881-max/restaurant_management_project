3de4062695d990baec953549c456ef297b459582
codewithanubhav <anubhav3881@gmail.com>
2026-04-01 19:29:12 +0530

    68949

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True) # coupon code
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2) # discount
+    is_active = models.BooleanField(default=True) # active ya inactive
+    valid_from = models.DateField() #start date
+    valid_until = models.DateField() #end date
+    def __str__(self):
+        return self.code
