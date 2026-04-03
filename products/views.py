e68594778123fa54578a7390fb42ea7ad9be6461
codewithanubhav <anubhav3881@gmail.com>
2026-04-03 21:24:07 +0530

    69111

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework import generics
 from .models import Item
 from .serializers import ItemSerializer
 
@@ -24,3 +24,7 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FeaturedItemView(generics.ListAPIView):
+    queryset = Item.objects.filter(is_featured=True)
+    serializer_class = ItemSerializer
