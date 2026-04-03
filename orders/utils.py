7819227e590f80a49dc1b8c90b6fb0c872b12cea
codewithanubhav <anubhav3881@gmail.com>
2026-04-03 21:39:32 +0530

    69112

--- orders/utils.py
+++ orders/utils.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from .models import DailyOperatingHours
+import random
+import string
 
 def get_today_operating_hours():
     today = datetime.today().strftime('%A')
@@ -9,3 +11,13 @@ def get_today_operating_hours():
         return (hours.open_time, hours.close_time)
         except DailyOperatingHours.DoesNoteExist:
             return (None, None)
+
+def generate_coupon_code(length=10):
+    characters = string.ascii_uppercase + string.digits
+    while True:
+        code = ''.join(random.choice(characters) for _ in range(length))
+        # Yaha check karna hai ki code unique hai ya nahi
+        # Example (agar Coupon model hai):
+        # if not Coupon.objects.filter(code=code).exists():
+        # return code
+        return code # (agar abhi DB check nahi hai to simple return kar do)
