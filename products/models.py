ee8bcc0e6c401356847d09d99d77e594c239c5cf
codewithanubhav <anubhav3881@gmail.com>
2026-04-03 21:21:23 +0530

    69111

--- products/models.py
+++ products/models.py
@@ -5,6 +5,10 @@ class Item(models.Model):
     item_name = models.CharField(max_length=150)
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_featured = models.DateTimeField(default=False)
+    # Now run the command
+    # python manage.py makemigrations
+    # python manahe.py migrate
 
     def __str__(self):
         return str(self.item_name)
