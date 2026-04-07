200b8e3387da164208cfd15ca94c0fff302ae0c9
codewithanubhav <anubhav3881@gmail.com>
2026-04-07 20:33:38 +0530

    69343

--- home/models.py
+++ home/models.py
@@ -6,3 +6,15 @@ from django.db import models
 class MenuCategory(models.Model):
     # name field jisme category ka naam store hoga (unque hona chahiye)
     name = models.CharField(max_length=100, unque=True)
+
+class DailySpecial(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    @staticmethod
+    def get_random_special():
+        return DailySpecial.objects.order_by('?').first()
+    
+    def __str__(self):
+        return self.name
