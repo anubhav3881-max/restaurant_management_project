0553e63dfef54ed3ddd421126ddf7e8a018bf3e6
codewithanubhav <anubhav3881@gmail.com>
2026-04-08 02:06:06 +0530

    69346

--- products/models.py
+++ products/models.py
@@ -24,4 +24,16 @@ class Restaurant(models.Model):
     # py manage.py makemigrations
     # py manage.py migrate
 
-    
+class NutritionalInformation(models.Model):
+    menu_item = models.ForeignKey('Item', on_delete=models.CASCADE)
+    calories = models.IntegerField()
+    protein_grams =models.DecimalField(max_digits=5, decimal_places=2)
+    fat_grams = models.DecimalField(max_digits=5, decimal_places=2)
+    carbohydrate_grams = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.menu_item.item_name} - {self.calories} calories"
+
+# Now run the command
+# python manage.py makemigrations
+# python manage.py migrate   
