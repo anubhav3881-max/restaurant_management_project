d62754589fe129baf5b7a4e3989b822602c8a1c9
codewithanubhav <anubhav3881@gmail.com>
2026-04-14 14:46:47 +0530

    70144

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Item, Ingredient
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
+        fields = '__all__'
+
+class IngredientSerializer(serializers. ModelSerializer):
+    class Meta:
+        model = Ingredient
         fields = '__all__'
