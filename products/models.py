046f74f5ce94be5cc023b54a573c16babb607ffe
codewithanubhav <anubhav3881@gmail.com>
2026-04-14 15:01:00 +0530

    70144

--- products/models.py
+++ products/models.py
@@ -6,6 +6,7 @@ class Item(models.Model):
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     is_featured = models.DateTimeField(default=False)
+    Ingredients = models.ManyTOManyField(Ingredient)
     # Now run the command
     # python manage.py makemigrations
     # python manahe.py migrate
@@ -37,3 +38,9 @@ class NutritionalInformation(models.Model):
 # Now run the command
 # python manage.py makemigrations
 # python manage.py migrate   
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
