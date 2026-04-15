9ce2be9f196f7d884c32ed1abca8808c04e0f624
codewithanubhav <anubhav3881@gmail.com>
2026-04-15 11:55:56 +0530

    70145

--- account/views.py
+++ account/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .utils import validate_phone_number
 # Create your views here.
+
+phone = request.POST.get('phone')
+
+if not validate_phone_number(phone):
+    return HttpResponse("Invalid phone number")
