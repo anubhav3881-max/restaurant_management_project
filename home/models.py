30cb64eca19b61a4e8e5c6e72dd6b1253f5631a4
codewithanubhav <anubhav3881@gmail.com>
2026-04-15 12:10:54 +0530

    70146

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 # Django models import
 # Create your models here.
 
@@ -7,10 +8,17 @@ class MenuCategory(models.Model):
     # name field jisme category ka naam store hoga (unque hona chahiye)
     name = models.CharField(max_length=100, unque=True)
 
+class DailySpecialManager(models.Manager):
+    def upcoming(self):
+        today = datetime.date.today()
+        return self.filter(special_date__gte=today)
+
 class DailySpecial(models.Model):
     name = models.CharField(max_length=100, verbose_name="Category Name")
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    special_date = model.DateField()
+    objects = DailySpecialManager()
     
     @staticmethod
     def get_random_special():
