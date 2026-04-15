db6361e5bda7aca360e7f048b4dc8c229957b627
codewithanubhav <anubhav3881@gmail.com>
2026-04-15 12:27:29 +0530

    70147

--- products/urls.py
+++ products/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from .views import ItemView, FeaturedItemView, ItemViewSet, ItemIngredientsView
+from .views import ItemView, FeaturedItemView, ItemViewSet, ItemIngredientsView, MenuItemViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('items', ItemViewSet)
+router.register('menu', MenuItemViewSet)
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('featured-items/', FeaturedItemView.as_view()),
     path('items/<int:pk/ingredients/', ItemIngredientsView.as_view()),
+
 ]
 
 urlpatterns += router.urls
