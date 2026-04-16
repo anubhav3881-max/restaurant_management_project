c6a7d2199afcec8290349eedc6d52813aa43d5a7
codewithanubhav <anubhav3881@gmail.com>
2026-04-16 22:19:55 +0530

    70471

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import * #view import
+from .views import *
+from .views import MenuCategoryListView, TableDetailView #view import
 
 urlpatterns = [
     path('menu-categories/', MenuCategoryListView.as_view()),
     # Ye URL hit karne par sab categories ka data milega
-    path('api/tables/<int:pk/', TableDetailView.as_view()),
+    path('api/tables/<int:pk>/', TableDetailView.as_view()),
 ]
