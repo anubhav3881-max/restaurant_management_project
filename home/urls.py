472f2b264c4b4c7e94e3d20ce7afe44b53fa2b76
codewithanubhav <anubhav3881@gmail.com>
2026-04-16 22:17:06 +0530

    70471

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from .views import * #view import
 urlpatterns = [
     path('menu-categories/', MenuCategoryListView.as_view()),
     # Ye URL hit karne par sab categories ka data milega
+    path('api/tables/<int:pk/', TableDetailView.as_view()),
 ]
